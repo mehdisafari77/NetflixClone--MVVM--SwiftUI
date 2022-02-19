d351745288beea283d47b2b42c2060cd24f4e3b8
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-02-18 17:59:38 -0800

    Initial Commit

---
+++ NetflixClone-(MVVM)-SwiftUI/NetflixClone__MVVM__SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixClone__MVVM__SwiftUIApp.swift
+//  NetflixClone-(MVVM)-SwiftUI
+//
+//  Created by Mehdi MMS on 18/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct NetflixClone__MVVM__SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
