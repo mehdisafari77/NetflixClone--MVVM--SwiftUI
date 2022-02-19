a5e4913ffb2df783ea6f6c1daa56fdcca90a3001
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-02-18 20:02:16 -0800

    add initial designing of large moview previw for top page

---
+++ NetflixClone-(MVVM)-SwiftUI/Home/TopMoviePreview.swift
@@ -0,0 +1,62 @@
+//
+//  TopMoviePreview.swift
+//  NetflixClone-(MVVM)-SwiftUI
+//
+//  Created by Mehdi MMS on 18/02/2022.
+//
+
+import SwiftUI
+import KingfisherSwiftUI
+
+struct TopMoviePreview: View {
+    
+    var movie: Movie
+    
+    func isCategoryLast(_ cat: String) -> Bool {
+        let catCount = movie.categories.count
+
+        if let index  = movie.categories.firstIndex(of: cat) {
+            if index + 1 != catCount {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    var body: some View {
+        ZStack {
+            KFImage(movie.thumbnailURL)
+                .resizable()
+                .scaledToFill()
+                .clipped()
+            
+            VStack {
+                Spacer()
+                
+                HStack {
+                    ForEach(movie.categories, id: \.self) { category in
+                        
+                        HStack {
+                            Text(category)
+                            
+                            if !isCategoryLast(category) {
+                                Image(systemName: "circle.fill")
+                                    .foregroundColor(.blue)
+                                    .font(.system(size:3))
+                            }
+                        }
+                    }
+                }
+                Text("Row of Buttons")
+            }
+        }
+        .foregroundColor(.white)
+    }
+}
+
+struct TopMoviePreview_Previews: PreviewProvider {
+    static var previews: some View {
+        TopMoviePreview(movie: exampleMovie1)
+    }
+}
