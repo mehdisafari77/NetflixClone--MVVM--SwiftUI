5bec35add3a13092ee38c9f9fd1b394a781a7912
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-02-18 19:11:53 -0800

    finished final code design of the initial h and vstacks in the home view, scrolling left and right plus the movie models and global helpers to show the images and titles

--- NetflixClone-(MVVM)-SwiftUI/Home/HomeVM.swift
+++ NetflixClone-(MVVM)-SwiftUI/Home/HomeVM.swift
@@ -11,4 +11,26 @@ class HomeVM: ObservableObject {
     
     // String == Category
     @Published var movies: [String: [Movie]] = [:]
+    
+    public var allCategories: [String] {
+        movies.keys.map({ String($0) })
+    }
+    
+    public func getMovie(forCat cat: String) -> [Movie] {
+        return movies[cat] ?? []
+    }
+    
+    init() {
+        setupMovies()
+    }
+    
+    func setupMovies() {
+        movies["Trending Now"] = exampleMovies
+        movies["Stand-Up Comedy"] = exampleMovies.shuffled()
+        movies["New Releases"] = exampleMovies.shuffled()
+        movies["Watch It Again"] = exampleMovies.shuffled()
+        movies["Sci-Fi Movies"] = exampleMovies.shuffled()
+
+
+    }
 }
--- NetflixClone-(MVVM)-SwiftUI/Home/HomeView.swift
+++ NetflixClone-(MVVM)-SwiftUI/Home/HomeView.swift
@@ -8,13 +8,40 @@
 import SwiftUI
 
 struct HomeView: View {
-    
+    var vm = HomeVM()
     
     var body: some View {
         ZStack {
             Color.black
                 .edgesIgnoringSafeArea(.all)
+            
+            // main VStack
+            ScrollView(showsIndicators: false) {
+                LazyVStack {
+                    
+                    ForEach(vm.allCategories, id: \.self) { category in
+                        VStack {
+                            HStack {
+                                Text(category)
+                                    .font(.title3)
+                                    .bold()
+                                Spacer()
+                            }
+                            
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                LazyHStack {
+                                    ForEach(vm.getMovie(forCat: category)) { movie in StandardHomeMovie(movie: movie)
+                                            .frame(width: 100, height: 200)
+                                            .padding(.horizontal, 20)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
+        .foregroundColor(.white)
     }
 }
 
