70cae2b6108b254adcb1c9e828ecbd6f59d98547
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-10 19:13:55 -0800

    random Movie generation

--- NetflixClone-(MVVM)-SwiftUI/Search/SearchVM.swift
+++ NetflixClone-(MVVM)-SwiftUI/Search/SearchVM.swift
@@ -12,4 +12,44 @@ class SearchVM: ObservableObject {
     
     @Published var isLoading: Bool = false
     
+    @Published var viewState: ViewState = ViewState.ready
+    
+    @Published var popularMovies: [Movie] = []
+    @Published var seachResults: [Movie] = []
+    
+    @Published var isShowingPopularMovies = true
+    
+    init() {
+        getPopularMovies()
+    }
+    
+    public func updateSearchText(with text: String) {
+        setViewState(to: .loading)
+        
+        getSearchResults(forText: text)
+    }
+    
+    private func getPopularMovies() {
+        
+    }
+    
+    private func getSearchResults(forText text: String) {
+        
+    }
+    
+    private func setViewState(to state: ViewState) {
+        DispatchQueue.main.async {
+            self.viewState = state
+            self.isLoading = state == .loading
+            
+        }
+    }
+    
+}
+
+enum ViewState {
+    case empty
+    case loading
+    case ready
+    case error
 }
