b37c50a13ea413bea5f6e5a955c51a187e615f6b
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-10 16:08:48 -0800

    done with searchBar

--- NetflixClone-(MVVM)-SwiftUI/Helpers/GlobalHelpers.swift
+++ NetflixClone-(MVVM)-SwiftUI/Helpers/GlobalHelpers.swift
@@ -196,3 +196,9 @@ extension String {
         return size.width
     }
 }
+
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
---
+++ NetflixClone-(MVVM)-SwiftUI/Search/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  NetflixClone-(MVVM)-SwiftUI
+//
+//  Created by Mehdi MMS on 10/03/2022.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
